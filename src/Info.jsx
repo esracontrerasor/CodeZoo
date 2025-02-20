5948eb35b69e6225470ffe6263dc1fc48dab9086
Alan Cuevas <alcuevasal@ittepic.edu.mx>
2025-02-19 21:12:35 -0700

    se solucionaron los problemas de bug del boton de iniciar de info add y se modifico el css de App para que no hubiera problemas con las dimensiones de los contenedores

--- src/Info.jsx
+++ src/Info.jsx
@@ -10,39 +10,40 @@ const Info = () => {
       };
 
   return (
-    <body>
-      <header class = "header"> 
-          <div className="logo">
-            <h1>CodeZoo</h1>
-          </div>
-          <nav>
-            <ul class="nav-links">
-              <li><a href="#">Experiencia más segura</a></li>
-              <li><a href="#">Sugerencias</a></li>
-              <li><a href="#">Juegos diversos</a></li>
-            </ul>
-          </nav>
-          <button className="btn" onClick={handleAccept}>INICIAR</button>
-      </header>
-
-      <div className="contenido-principal">
-
-          <div className="info-box">
-            <h2 className="title">Una aplicación pensada para los pequeños</h2>
-            <p className="text">
-              CodeZoo se creó con la idea de ofrecer un entorno educativo
-              interactivo para menores en el que pueden aprender programación fácil
-              y divertida. Además, ayuda a sus padres a guiarlos mientras aprenden
-              y desarrollan nuevos conocimientos.
-            </p>
-          </div>
-
-          <div className="imagen-info">
-           
-          </div>
-        </div>
       
-    </body>     
+    <div className="contenedor-info">
+
+        <header class = "header"> 
+                <div className="logo">
+                  <h1>CodeZoo</h1>
+                </div>
+                <nav>
+                  <ul class="nav-links">
+                    <li><a href="#">Experiencia más segura</a></li>
+                    <li><a href="#">Sugerencias</a></li>
+                    <li><a href="#">Juegos diversos</a></li>
+                  </ul>
+                </nav>
+                <button className="btn" onClick={handleAccept}>INICIAR</button>
+        </header>
+
+        <div className="contenido-principal">
+
+            <div className="info-box">
+              <h2 className="title">Una aplicación pensada para los pequeños</h2>
+              <p className="text">
+                CodeZoo se creó con la idea de ofrecer un entorno educativo
+                interactivo para menores en el que pueden aprender programación fácil
+                y divertida. Además, ayuda a sus padres a guiarlos mientras aprenden
+                y desarrollan nuevos conocimientos.
+              </p>
+            </div>
+
+            <div className="imagen-info">
+            
+            </div>
+          </div>
+      </div> 
   );
 };
 
