a57092cc71e697a2e1a99e0847fa7d5565fc472d
Alan Cuevas <alcuevasal@ittepic.edu.mx>
2025-04-30 20:23:30 -0700

    implementacion al iniciar sesion aparezca foto de perfil y menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.css";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import userIcon from "../../resources/user-icon-white.png";
+import bxUser from "../../resources/icons/bx-user.svg";
+import bxLogOut from "../../resources/icons/bx-log-out.svg";
+import bxForo from "../../resources/icons/bx-chat.svg";
 
 
 const Navbar = ({onLoginClick}) => {
+    const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            setIsAuthenticated(true);
+        } else {
+            setIsAuthenticated(false);
+        }
+    }, []);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        const token = localStorage.getItem("token");
+
+        if(token){
+            navigate("/home");
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Acceso denegado",
+                text: "Debes iniciar sesión para acceder a esta página",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    onLoginClick();
+                }
+            })
+        }
+      
+    }
+
+    const handleLogOut = () => {
+        localStorage.removeItem("token");
+        setIsAuthenticated(false);
+        navigate("/");
+    }
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    const goTo = (path) => {
+        navigate(path);
+        setMenuOpen(false);
+    }
+
     return (
         <header className="header">
             <a href="/" className="logo">CodeZoo</a>
 
             <nav className="navbar">
                 <a href="/">Inicio</a>
-                <a href="/home">Actividades</a>
+                <a href="#" onClick={handleClick}>Actividades</a>
                 <a href="/info">Acerca De</a>
-                <button className="btnLogin" onClick={onLoginClick}>Ingresar</button>
+                {isAuthenticated ? (
+                   <div className="user-menu">
+                        <img src={userIcon} className="user-icon" alt="" onClick={toggleMenu}/>
+                   {menuOpen && (
+                    <div className={`menu-dropdown ${menuOpen ? "active" : ""}`}>
+                        <ul>
+                            <li onClick={() => goTo("/perfil")}> Ver mi perfil <img src={bxUser} className="icono-menu" /> </li>
+                            <li onClick={() => goTo("/foro")}>Foro de ayuda <img src={bxForo} className="icono-menu" /></li>
+                            <li onClick={handleLogOut}>Cerrar sesión <img src={bxLogOut} className="icono-menu" /></li>
+                        </ul>
+                    </div>
+                   )}
+                   </div>
+                ) : (
+                    <button className="btnLogin" onClick={onLoginClick}>Ingresar</button>
+                )}
+
             </nav>
         </header>
     );
