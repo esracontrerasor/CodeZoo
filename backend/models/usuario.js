e04d7782e33536439fb1ad6a405e58f91fede37e
Alan Cuevas <alcuevasal@ittepic.edu.mx>
2025-04-30 20:30:50 -0700

    un pequeño ajusto al modelo de usuario

--- backend/models/usuario.js
+++ backend/models/usuario.js
@@ -1,5 +1,6 @@
 import * as mongoose from 'mongoose';
 
+
 const usuarioSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
